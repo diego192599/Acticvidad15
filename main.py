02169ea8983b7b9819740fbc24e7b004bd2d86d0
diego192599 <di.cramire@udeo.edu.gt>
2025-08-11 11:27:05 -0600

    creacion de metodo de agregar estudiante y sus excepciones

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Menu")
+estudiantes={}
+def agregar_Estudiantes():
+    print("==Agregar estudiante==")
+    cantidad=int(input("Ingrese la cantidad de estudiantes a agregar: "))
+    while True:
+        try:
+            if cantidad == 0:
+                print("La cantidad ingresada no es valida vuelva a intentar")
+                break
+            elif cantidad<0:
+                print("La cantidad no puede ser menor a 0")
+            else:
+                for i in range(cantidad):
+                    nombre=input(f"Ingrese el nombre del estudiante #{i+1}")
+                    if nombre:
+                        estudiantes[nombre]={}
+                        print(f"El {nombre} se ha guardado correctamente")
+                    else:
+                        print("El nombre no puede quedar vacio intente nuevamente")
+                break
+        except ValueError
